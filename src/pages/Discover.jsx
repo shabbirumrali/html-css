3739dbf682f0f777b3ba3ec5ac7e6ce1add0a375
shabbirumrali <shabbirumrali@gmail.com>
2022-12-21 19:33:17 +0530

    added song card run api using redux apply icons

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,50 @@
-const Discover = () => <div>Discover</div>;
+import { useDispatch, useSelector } from 'react-redux'
+import { Error, Loader, SongCard } from '../components'
+import { genres } from '../assets/constants'
+
+import { useGetAlbumsQuery } from '../redux/services/youtubeMusic'
+
+const Discover = () => {
+    const dispatch = useDispatch()
+    const { activeSong, isPlaying } = useSelector((state) => state.player)
+    const { data, isFetching, error } = useGetAlbumsQuery()
+    const genreTitle = 'Pop'
+    console.log(data && data)
+
+    if (isFetching) return <Loader title={'loading songs...'} />
+
+    if(error) return <Error /> 
+
+    return (
+        <div className='flex flex-col'>
+            <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+                <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
+                <select
+                    onChange={() => {}}
+                    value=""
+                    className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none mt-5 sm:mt-0'
+                >
+                    { genres.map(genre => (
+                        <option key={genre.value} value={genre.value}>{genre.title}</option>
+                    )) }
+                </select>
+            </div>
+
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {data && data.result.songs.map((song, i) => (
+                    <SongCard 
+                        key={song.id}
+                        song={song}
+                        thumbnail={data.result.thumbnail}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
+                        i={i}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
 
 export default Discover;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { youtubeMusicApi } from './services/youtubeMusic';
 
 export const store = configureStore({
   reducer: {
+    [ youtubeMusicApi.reducerPath ]: youtubeMusicApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(youtubeMusicApi.middleware)
 });
+ 
